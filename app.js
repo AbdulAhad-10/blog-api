17d5eb40c2a1a505431353fc9b939a9274c4fba9
Abdul Ahad <ahad55056@gmail.com>
2025-08-12 10:16:23 +0500

    deploying on vercel

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
+// import express from "express";
+// import { PORT } from "./config/env.js";
+// import cookieParser from "cookie-parser";
+// import authRouter from "./routes/auth.routes.js";
+// import blogRouter from "./routes/blog.routes.js";
+// import userRouter from "./routes/user.routes.js";
+// import connectToDatabase from "./database/mongodb.js";
+// import errorMiddleware from "./middleware/error.middleware.js";
+// import arcjetMiddleware from "./middleware/arcjet.middleware.js";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(arcjetMiddleware);
+
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/blogs", blogRouter);
+// app.use("/api/v1/users", userRouter);
+
+// app.use(errorMiddleware);
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the Blog API!");
+// });
+
+// app.listen(PORT, async () => {
+//   console.log(`Blog API is running on port http://localhost:${PORT}`);
+
+//   await connectToDatabase();
+// });
+
 import express from "express";
-import { PORT } from "./config/env.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import blogRouter from "./routes/blog.routes.js";
@@ -9,6 +41,8 @@ import errorMiddleware from "./middleware/error.middleware.js";
 import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 
 const app = express();
+
+await connectToDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,8 +59,4 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Blog API!");
 });
 
-app.listen(PORT, async () => {
-  console.log(`Blog API is running on port http://localhost:${PORT}`);
-
-  await connectToDatabase();
-});
+export default app;
